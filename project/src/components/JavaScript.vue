6a334f25a4f8cc57a7c17f35889815a63cfd928c
pingzina <1241860821@qq.com>
2019-01-18 14:36:58 +0800

    添加新的组件JavaScript

---
+++ project/src/components/JavaScript.vue
@@ -0,0 +1,22 @@
+<template>
+      <div class="javascript">
+
+        <p>this is one text</p>
+      </div>
+</template>
+
+<script>
+    export default {
+        name: "JavaScript",
+        data(){
+          return{
+
+          }
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
